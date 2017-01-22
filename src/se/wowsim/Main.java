c634c9c12735a51038ea7f26ae206960d82469e5
Dewin <simon@temtron.se>
2017-01-22 23:20:33 +0100

    here is testing, buy yeah we are shooting some shadowbolts now

--- src/se/wowsim/Main.java
+++ src/se/wowsim/Main.java
@@ -1,16 +1,22 @@
 package se.wowsim;
 
 import se.wowsim.spells.Corruption;
+import se.wowsim.spells.Shadowbolt;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Target target = new Target();
-		Corruption corruption = new Corruption(target);
-		
-		for(int i = 1; i <= 180; i ++) {
-			if(i % 10 == 0) { System.out.println("second: " + i/10); }
-			target.notifyObservers();
-		}
-	}
+    public static void main(String[] args) {
+
+        Target target = new Target();
+        new Corruption(target);
+        new Shadowbolt(target);
+
+        for (int i = 1; i <= 180; i++) {
+            if (i % 10 == 0)
+                System.out.println("second: " + i / 10);
+            if (i == 60 || i == 150)
+                new Shadowbolt(target);
+
+            target.notifyObservers();
+        }
+    }
 }
