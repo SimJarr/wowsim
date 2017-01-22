207961022a487a18f7a182c94cf4b0b28815891d
Dewin <simon@temtron.se>
2017-01-22 23:20:09 +0100

    changed to CopyOnWriteArrayList so we can unregister while looping

--- src/se/wowsim/Target.java
+++ src/se/wowsim/Target.java
@@ -1,32 +1,35 @@
 package se.wowsim;
 
-import java.util.ArrayList;
+import se.wowsim.spells.DamageOverTime;
+
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Target implements Subject {
 
-	private List<Observer> observers;
-	
-	public Target() {
-		this.observers = new ArrayList<>();
-	}
-	
-	@Override
-	public void register(Observer o) {
-		observers.add(o);
-		System.out.println(o.getName() + " applied to target");
-	}
+    private List<Observer> observers;
 
-	@Override
-	public void unregister(Observer o) {
-		int index = observers.indexOf(o);
-		observers.remove(index);		
-	}
+    public Target() {
+        this.observers = new CopyOnWriteArrayList<>();
+    }
 
-	@Override
-	public void notifyObservers() {
-		for(Observer o : observers) {
-			o.update();
-		}
-	}
+    @Override
+    public void register(Observer o) {
+        observers.add(o);
+        if(o instanceof DamageOverTime)
+            System.out.println(o.getName() + " applied to target");
+    }
+
+    @Override
+    public void unregister(Observer o) {
+        int index = observers.indexOf(o);
+        observers.remove(index);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
 }
