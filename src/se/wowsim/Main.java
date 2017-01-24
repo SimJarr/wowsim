5fd5ed777723b6614d27d7f36f6fd5fd782fc6df
Dewin <simon@temtron.se>
2017-01-24 22:43:27 +0100

    made it easier to create classes

--- src/se/wowsim/Main.java
+++ src/se/wowsim/Main.java
@@ -2,16 +2,18 @@ package se.wowsim;
 
 import se.wowsim.classes.ClassBuilder;
 import se.wowsim.classes.Classes;
+import se.wowsim.classes.Priest;
 import se.wowsim.classes.Warlock;
 
 public class Main {
 	public static void main(String[] args) {
 		
 
-		Warlock warlock = (Warlock) new ClassBuilder(Classes.WARLOCK, 32, 95).getClassInstance();
+		//Warlock warlock = (Warlock) new ClassBuilder(Classes.WARLOCK, 32, 95).getClassInstance();
+        Priest jarre = (Priest) new ClassBuilder(Classes.PRIEST, 31, 1000).getClassInstance();
 		Target target = new Target();
 
-        Simulator.simulate(warlock, target, 400);
+        Simulator.simulate(jarre, target, 400);
 
         System.out.println("Total Damage Done: " + Simulator.getTotalDamageDone());
         System.out.println("Dps: " + Simulator.getDps());
--- src/se/wowsim/classes/ClassBuilder.java
+++ src/se/wowsim/classes/ClassBuilder.java
@@ -7,50 +7,60 @@ import java.util.Set;
 import se.wowsim.spells.*;
 
 public class ClassBuilder {
-	
-	private Classes myClass;
-	private int level;
-	private int intellect;
-	private Set<Spell> allSpells;
-	
-	public ClassBuilder(Classes myClass, int level, int intellect) {
-		this.myClass = myClass;
-		this.level = level;
+
+    private Classes myClass;
+    private int level;
+    private int intellect;
+    private Set<Spell> allSpells;
+
+    public ClassBuilder(Classes myClass, int level, int intellect) {
+        this.myClass = myClass;
+        this.level = level;
         this.intellect = intellect;
-		allSpells = new HashSet<>();
-		importSpells();
-	}
-	
-	public ClassTemplate getClassInstance() {
-		switch (myClass) {
-		case WARLOCK:
-			Warlock warlock = new Warlock(intellect);
-			for(Spell s : allSpells) {
-				if(s.getSpellClass().equals(Classes.WARLOCK)) {
-					warlock.addSpell(s.getName(), s);
-                    if (s instanceof DirectDamage){
-                        ((DirectDamage) s).setCritChance(myClass.calculateCritChance(intellect, level));
-                    }
-                    s.init();
-				}
-			}
-			return warlock;
-		default:
-			return null;
-		}
-	}
-	
-	private void importSpells() {
-		allSpells.add(new Corruption(determineSpellLevel(Corruption.levelUps)));
-		allSpells.add(new CurseOfAgony(determineSpellLevel(CurseOfAgony.levelUps)));
-		allSpells.add(new Shadowbolt(determineSpellLevel(Shadowbolt.levelUps)));
-	}
-	
-	private int determineSpellLevel(List<Integer> levelups) {
-		int levelOfSpell = 0;
-		for(int i : levelups) {
-			if (this.level >= i) { levelOfSpell++; }
-		}
-		return levelOfSpell;
-	}
+        allSpells = new HashSet<>();
+        importSpells();
+    }
+
+    public ClassTemplate getClassInstance() {
+        switch (myClass) {
+            case PRIEST:
+                Priest priest = new Priest(intellect);
+                addSpellsToMyClass(priest);
+                return priest;
+            case WARLOCK:
+                Warlock warlock = new Warlock(intellect);
+                addSpellsToMyClass(warlock);
+                return warlock;
+            default:
+                return null;
+        }
+    }
+
+    private void addSpellsToMyClass(ClassTemplate classTemplate) {
+        for (Spell s : allSpells) {
+            if (s.getSpellClass().equals(myClass)) {
+                classTemplate.addSpell(s.getName(), s);
+                if (s instanceof DirectDamage) {
+                    ((DirectDamage) s).setCritChance(myClass.calculateCritChance(intellect, level));
+                }
+                s.init();
+            }
+        }
+    }
+
+    private void importSpells() {
+        allSpells.add(new Corruption(determineSpellLevel(Corruption.levelUps)));
+        allSpells.add(new CurseOfAgony(determineSpellLevel(CurseOfAgony.levelUps)));
+        allSpells.add(new Shadowbolt(determineSpellLevel(Shadowbolt.levelUps)));
+    }
+
+    private int determineSpellLevel(List<Integer> levelups) {
+        int levelOfSpell = 0;
+        for (int i : levelups) {
+            if (this.level >= i) {
+                levelOfSpell++;
+            }
+        }
+        return levelOfSpell;
+    }
 }
