480e4cf3168d6c884af1ddd695681be0ab63a10a
Dewin <simon@temtron.se>
2017-01-24 22:43:48 +0100

    made it easier to create classes

---
+++ src/se/wowsim/classes/Priest.java
@@ -0,0 +1,11 @@
+package se.wowsim.classes;
+
+public final class Priest extends ClassTemplate {
+
+    public Priest(double intellect) {
+        super();
+        this.myClass = Classes.PRIEST;
+        this.intellect = intellect;
+    }
+
+}
