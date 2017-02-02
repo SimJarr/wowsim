ccbf3165d1702fcbc6e7432704d5c42083020241
Dewin <simon@temtron.se>
2017-02-02 22:20:12 +0100

    todo

--- src/se/wowsim/spells/types/Channeling.java
+++ src/se/wowsim/spells/types/Channeling.java
@@ -2,6 +2,8 @@ package se.wowsim.spells.types;
 
 public abstract class Channeling extends DamageOverTime {
 
+	//TODO be able to stop channeling
+
 	public Channeling(int rank) {
 		super(rank);
 		castTime = 0;
